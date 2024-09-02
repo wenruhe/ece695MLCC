26ee782393e0695872983c6afd3117581a741333
Wenru He <60372358+wenruhe@users.noreply.github.com>
2024-09-02 16:57:50 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,16 +1,28 @@
 import geni.portal as portal
 import geni.rspec.pg as rspec
 
-# Create a Request object to start building the RSpec.
-request = portal.context.makeRequestRSpec()
-# Create a XenVM
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-node.routable_control_ip = "true"
+# Create the first XenVM
+node1 = request.XenVM("node1")
+node1.disk_image = "urn:publicid:IDN+utah.cloudlab.us+image+ece69500mlcc-PG0:wenruhe"
+node1.routable_control_ip = "true"
 
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
-node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
+node1.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
+node1.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
+node1.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
+
+# Create the second XenVM
+node2 = request.XenVM("node2")
+node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+node2.routable_control_ip = "true"
+
+node2.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
+node2.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
+node2.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
+
+# Optionally, you can define a link between nodes
+link = request.Link()
+link.addInterface(node1.addInterface())
+link.addInterface(node2.addInterface())
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
